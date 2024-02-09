3f33b70d16473a5cc11b03b54f197387e5768267
kei-nishio <kei.nishio.031107@gmail.com>
2024-02-09 16:22:46 +0900

    add header.php

--- src/wp/header.php
+++ src/wp/header.php
@@ -14,88 +14,137 @@
 
 <body <?php body_class(); ?>>
   <?php wp_body_open(); ?>
-  <header class="header js-header">
-    <div class="header__inner">
+    <!-- Header -->
+    <header class="header">
+      <div class="header__inner">
+        <!-- logo -->
+        <h1 class="header__logo">
+          <a href="./" class="header__logo-link">
+            <img src="./assets/images/common/logo.svg" alt="コードアップス" />
+          </a>
+        </h1>
 
-      <!-- logo  -->
-      <h1 class="header__logo">
-        <a href="<?php echo esc_url(home_url('/')); ?>" class="header__logo-link">
-          <img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/logo1.svg" alt="ヘッダーロゴ" />
-        </a>
-      </h1>
+        <!-- navigation -->
+        <div class="header__pc-nav pc-nav u-desktop">
+          <ul class="pc-nav__items">
+            <li class="pc-nav__item">
+              <a href="./campaign.html" class="pc-nav__link">
+                <div class="pc-nav__main-text">campaign</div>
+                <div class="pc-nav__sub-text">キャンペーン</div>
+              </a>
+            </li>
+            <li class="pc-nav__item">
+              <a href="./about-us.html" class="pc-nav__link">
+                <div class="pc-nav__main-text">about us</div>
+                <div class="pc-nav__sub-text">私たちについて</div>
+              </a>
+            </li>
+            <li class="pc-nav__item">
+              <a href="./information.html" class="pc-nav__link">
+                <div class="pc-nav__main-text">information</div>
+                <div class="pc-nav__sub-text">ダイビング情報</div>
+              </a>
+            </li>
+            <li class="pc-nav__item">
+              <a href="./blog.html" class="pc-nav__link">
+                <div class="pc-nav__main-text">blog</div>
+                <div class="pc-nav__sub-text">ブログ</div>
+              </a>
+            </li>
+            <li class="pc-nav__item">
+              <a href="./voice.html" class="pc-nav__link">
+                <div class="pc-nav__main-text">voice</div>
+                <div class="pc-nav__sub-text">お客様の声</div>
+              </a>
+            </li>
+            <li class="pc-nav__item">
+              <a href="./price.html" class="pc-nav__link">
+                <div class="pc-nav__main-text">price</div>
+                <div class="pc-nav__sub-text">料金一覧</div>
+              </a>
+            </li>
+            <li class="pc-nav__item">
+              <a href="./faq.html" class="pc-nav__link">
+                <div class="pc-nav__main-text pc-nav__main-text--large">faq</div>
+                <div class="pc-nav__sub-text">よくある質問</div>
+              </a>
+            </li>
+            <li class="pc-nav__item">
+              <a href="./contact.html" class="pc-nav__link">
+                <div class="pc-nav__main-text">contact</div>
+                <div class="pc-nav__sub-text">お問合せ</div>
+              </a>
+            </li>
+          </ul>
+        </div>
 
-      <!-- navigation -->
-      <nav class="header__nav pc-nav u-desktop">
-        <ul class="pc-nav__items">
-          <li class="pc-nav__item">
-            <a href="<?php echo esc_url(home_url('/')); ?>" class="pc-nav__link">top</a>
-          </li>
-          <li class="pc-nav__item">
-            <a href="<?php echo esc_url(home_url('/about')); ?>" class="pc-nav__link">about</a>
-          </li>
-          <li class="pc-nav__item">
-            <a href="<?php echo esc_url(home_url('/service')); ?>" class="pc-nav__link">service</a>
-          </li>
-          <li class="pc-nav__item">
-            <a href="<?php echo esc_url(home_url('/works')); ?>" class="pc-nav__link">works</a>
-          </li>
-          <li class="pc-nav__item">
-            <a href="<?php echo esc_url(home_url('/news')); ?>" class="pc-nav__link">news</a>
-          </li>
-          <li class="pc-nav__item">
-            <a href="<?php echo esc_url(home_url('/contact')); ?>" class="pc-nav__link pc-nav__link--blue">
-              <i class="fa-regular fa-envelope"></i>
-              <span>contact</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
+        <!-- hamburger -->
+        <div class="header__hamburger hamburger u-mobile js-hamburger">
+          <span class="hamburger__border"></span>
+          <span class="hamburger__border"></span>
+          <span class="hamburger__border"></span>
+        </div>
 
-      <!-- hamburger -->
-      <div class="header__hamburger hamburger u-mobile js-hamburger">
-        <span class="hamburger__border"></span>
-        <span class="hamburger__border"></span>
-        <span class="hamburger__border hamburger__border--short"></span>
-      </div>
+        <!-- drawer -->
+        <div class="header__drawer u-mobile js-drawer-menu">
+          <div class="header__drawer-mask"></div>
+          <div class="header__drawer-inner">
+            <div class="header__sp-nav navigation">
+              <ul class="navigation__items">
+                <li class="navigation__item">
+                  <a href="./campaign.html">キャンペーン</a>
+                  <ul class="navigation__sub-items">
+                    <li class="navigation__sub-item"><a href="./campaign.html">ライセンス取得</a></li>
+                    <li class="navigation__sub-item"><a href="./campaign.html">貸切体験ダイビング</a></li>
+                    <li class="navigation__sub-item"><a href="./campaign.html">ナイトダイビング</a></li>
+                  </ul>
+                </li>
+                <li class="navigation__item"><a href="./about-us.html">私たちについて</a></li>
+              </ul>
 
-      <!-- drawer -->
-      <div class="header__drawer u-mobile js-drawer-menu">
-        <div class="header__drawer-header">
-          <div class="header__logo">
-            <div class="header__logo-link">
-              <img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/logo2.svg" alt="ヘッダーロゴ" />
+              <ul class="navigation__items">
+                <li class="navigation__item">
+                  <a href="./information.html">ダイビング情報</a>
+                  <ul class="navigation__sub-items">
+                    <li class="navigation__sub-item"><a href="./information.html?id=444">ライセンス講習</a></li>
+                    <li class="navigation__sub-item"><a href="./information.html?id=555">体験ダイビング</a></li>
+                    <li class="navigation__sub-item"><a href="./information.html?id=666">ファンダイビング</a></li>
+                  </ul>
+                </li>
+                <li class="navigation__item"><a href="./blog.html">ブログ</a></li>
+              </ul>
+
+              <ul class="navigation__items">
+                <li class="navigation__item">
+                  <a href="./voice.html">お客様の声</a>
+                </li>
+                <li class="navigation__item">
+                  <a href="./price.html">料金一覧</a>
+                  <ul class="navigation__sub-items">
+                    <li class="navigation__sub-item"><a href="./price.html#price01">ライセンス講習</a></li>
+                    <li class="navigation__sub-item"><a href="./price.html#price02">体験ダイビング</a></li>
+                    <li class="navigation__sub-item"><a href="./price.html#price03">ファンダイビング</a></li>
+                  </ul>
+                </li>
+              </ul>
+
+              <ul class="navigation__items">
+                <li class="navigation__item">
+                  <a href="./faq.html">よくある質問</a>
+                </li>
+                <li class="navigation__item">
+                  <a href="./privacy-policy.html">
+                    プライバシー
+                    <br />
+                    ポリシー
+                  </a>
+                </li>
+                <li class="navigation__item"><a href="./terms-of-service.html">利用規約</a></li>
+                <li class="navigation__item"><a href="./site-map.html">サイトマップ</a></li>
+                <li class="navigation__item"><a href="./contact.html">お問合わせ</a></li>
+              </ul>
             </div>
           </div>
         </div>
-        <div class="header__drawer-inner">
-          <nav class="header__sp-nav navigation">
-            <ul class="navigation__items">
-              <li class="navigation__item">
-                <a href="<?php echo esc_url(home_url('/')); ?>">top</a>
-              </li>
-              <li class="navigation__item">
-                <a href="<?php echo esc_url(home_url('/about')); ?>">about</a>
-              </li>
-              <li class="navigation__item">
-                <a href="<?php echo esc_url(home_url('/service')); ?>">service</a>
-              </li>
-              <li class="navigation__item">
-                <a href="<?php echo esc_url(home_url('/works')); ?>">works</a>
-              </li>
-              <li class="navigation__item">
-                <a href="<?php echo esc_url(home_url('/news')); ?>">news</a>
-              </li>
-              <li class="navigation__item">
-                <a href="<?php echo esc_url(home_url('/contact')); ?>">contact</a>
-              </li>
-              <li class="navigation__item">
-                <a href="<?php echo esc_url(home_url('/privacy-policy')); ?>">プライバシーポリシー</a>
-              </li>
-            </ul>
-          </nav>
-
-        </div>
       </div>
-      <div class="header__drawer-circle-bg js-header-circle"></div>
-    </div>
-  </header>
+    </header>
