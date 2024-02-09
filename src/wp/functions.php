f0fe81f84de8bd01b5bb3d9416bc070b6d6a7bd9
kei-nishio <kei.nishio.031107@gmail.com>
2024-02-09 22:35:33 +0900

    add キャンペーンカードのWP化完了

--- src/wp/functions.php
+++ src/wp/functions.php
@@ -40,19 +40,57 @@ function my_script_init()
 }
 
 // ! カスタム投稿の表示件数を設定する
-add_action('pre_get_posts', 'change_posts_per_page');
-function change_posts_per_page($query)
-{
-  if (is_admin() || !$query->is_main_query())
-    return;
-  if ($query->is_archive('works')) { //カスタム投稿タイプを指定
-    $query->set('posts_per_page', '6'); //表示件数を指定（-1は全件）
-  }
-}
+// add_action('pre_get_posts', 'change_posts_per_page');
+// function change_posts_per_page($query)
+// {
+//   if (is_admin() || !$query->is_main_query())
+//     return;
+//   if ($query->is_archive('works')) { //カスタム投稿タイプを指定
+//     $query->set('posts_per_page', '6'); //表示件数を指定（-1は全件）
+//   }
+// }
 
 // ! Contact Form 7で自動挿入されるPタグ、brタグを削除
 add_filter('wpcf7_autop_or_not', 'wpcf7_autop_return_false');
 function wpcf7_autop_return_false()
 {
   return false;
+}
+
+// ! カスタム投稿タイプのラベルを変更する
+add_action('init', 'Change_objectlabel');
+add_action('admin_menu', 'Change_menulabel');
+function Change_menulabel()
+{
+  global $menu;
+  global $submenu;
+  $name = 'ブログ';
+  $menu[5][0] = $name;
+  $submenu['edit.php'][5][0] = $name . '一覧';
+  $submenu['edit.php'][10][0] = '新しい' . $name;
+}
+function Change_objectlabel()
+{
+  global $wp_post_types;
+  $name = 'イベント';
+  $labels = &$wp_post_types['post']->labels;
+  $labels->name = $name;
+  $labels->singular_name = $name;
+  $labels->add_new_item = $name . 'の新規追加';
+  $labels->edit_item = $name . 'の編集';
+  $labels->new_item = '新規' . $name;
+  $labels->view_item = $name . 'を表示';
+  $labels->search_items = $name . 'を検索';
+  $labels->not_found = $name . 'が見つかりませんでした';
+  $labels->not_found_in_trash = 'ゴミ箱に' . $name . 'は見つかりませんでした';
+}
+
+// ! 個別記事なし投稿の場合リダイレクトする
+add_action('template_redirect', 'custom_redirect_custom_post_types');
+function custom_redirect_custom_post_types()
+{
+  if (is_singular('campaign', 'voice')) {
+    wp_redirect(home_url('/'), 302);
+    exit;
+  }
 }
