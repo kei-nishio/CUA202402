1045d8fed054adcc45bcb2a14a386529cb581f00
kei-nishio <kei.nishio.031107@gmail.com>
2024-02-22 22:39:30 +0900

    Fix: Updated single blog template to display posts correctly.*** (***修正: シングルブログテンプレートを更新して記事を正しく表示するようにしました。

--- src/wp/parts/page/p-single-blog.php
+++ src/wp/parts/page/p-single-blog.php
@@ -1,21 +1,27 @@
 <article class="page-blog__article">
-  <?php
-  $post_id = get_the_ID(); // 投稿の ID を指定
-  $thumbnail_id = get_post_thumbnail_id($post_id); // アイキャッチ画像の ID を取得
-  $alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true); // アイキャッチ画像の alt 属性を取得
-  ?>
-  <time class="page-blog__date" datetime="<?php the_time("c"); ?>"><?php the_time("Y.m/d"); ?></time>
-  <h2 class="page-blog__title"><?php the_title(); ?></h2>
-  <figure class="page-blog__image">
-    <?php if (has_post_thumbnail()) : ?>
-      <img src="<?php the_post_thumbnail_url("full"); ?>" alt="<?php echo esc_attr($alt); ?>">
-    <?php else : ?>
-      <img src="<?php echo esc_url(get_theme_file_uri("/assets/images/common/noimage.jpg")); ?>" alt="ノーイメージ画像" />
-    <?php endif; ?>
-  </figure>
-  <div class="page-blog_content">
-    <?php the_content(); ?>
-  </div>
+  <?php if (have_posts()) : ?>
+    <?php while (have_posts()) : the_post(); ?>
+      <?php
+      $post_id = get_the_ID(); // 投稿の ID を指定
+      $thumbnail_id = get_post_thumbnail_id($post_id); // アイキャッチ画像の ID を取得
+      $alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true); // アイキャッチ画像の alt 属性を取得
+      ?>
+      <time class="page-blog__date" datetime="<?php the_time("c"); ?>"><?php the_time("Y.m/d"); ?></time>
+      <h2 class="page-blog__title"><?php the_title(); ?></h2>
+      <figure class="page-blog__image">
+        <?php if (has_post_thumbnail()) : ?>
+          <img src="<?php the_post_thumbnail_url("full"); ?>" alt="<?php echo esc_attr($alt); ?>">
+        <?php else : ?>
+          <img src="<?php echo esc_url(get_theme_file_uri("/assets/images/common/noimage.jpg")); ?>" alt="ノーイメージ画像" />
+        <?php endif; ?>
+      </figure>
+      <div class="page-blog_content">
+        <?php the_content(); ?>
+      </div>
+    <?php endwhile; ?>
+  <?php else : ?>
+    <li>記事が投稿されていません</li>
+  <?php endif; ?>
 </article>
 
 <!-- Pagination -->
